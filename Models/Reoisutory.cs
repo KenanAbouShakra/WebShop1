75ee9273d1fd4468e128648e27099343e7052ad3
KenanAbouShakra <aboawskenan@gmail.com>
2025-12-12 12:54:42 +0100

    Add Repository.cs

---
+++ Models/Reoisutory.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebShop1.Data;
+
+namespace WebShop1.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context {  get; set; }
+        private DbSet<T> _dbSet { get; set; }
+        public Repository(AppDbContext context){
+            _context = context;
+            _dbSet = context.Set<T>();
+        
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, QueryOptions<T> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
