67f187591918f0d8f41932817f3365ead6a15569
KenanAbouShakra <aboawskenan@gmail.com>
2025-12-12 11:34:13 +0100

    Queery Options

---
+++ Models/QueryOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+namespace WebShop1.Models
+{
+    public class QueryOptions<T> where T : class
+    {
+        public Expression<Func<T, Object>> OrderBy { get; set; } = null;
+        public Expression<Func<T, bool>> Where { get; set; } = null!;
+        private string[] inclodes = Array.Empty<string>();
+
+        public string Inclodes
+        {
+            set => inclodes = value.Replace("", "").Split(',');
+        }
+        public string[] GetIncludes() => inclodes;
+        public bool HasWhere=> Where != null;
+        public bool HasOrderBy => OrderBy != null;
+
+
+    }
+}
